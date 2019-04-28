da207dd0bb2bbee08911ae611fb303579079171d
Zhi Hao <CurryYuanZH@outlook.com>
2019-04-29 01:00:11 +0800

    update home

--- pages/index/index.js
+++ pages/index/index.js
@@ -13,7 +13,10 @@ Page({
     interval: 2000,
     duration: 500,
     previousMargin: 0,
-    nextMargin: 0
+    nextMargin: 0,
+    iconList: [{icon:'brush_fill',name:'问卷'},{icon:'document_fill',name:'信息收集'},{icon:'group_fill',name:'招募'},{icon:'service_fill',name:'物流'}],
+    taskList: [{title:'task1',time:'time1',location:'location1',price:'1'},
+              {title:'task1',time:'time1',location:'location1',price:'1'}]
   },
 
   //事件处理函数
