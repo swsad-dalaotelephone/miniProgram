d4e93392aeed62c5a5c34b8648143017b1b9d320
CurryYuan <1172248391@qq.com>
2019-04-28 22:59:34 +0800

    add swiper

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,15 @@ const app = getApp()
 
 Page({
   data: {
-
+    background: ['../../images/home1.jpg', '../../images/home2.png', '../../images/home3.jpg'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: false,
+    circular: false,
+    interval: 2000,
+    duration: 500,
+    previousMargin: 0,
+    nextMargin: 0
   },
 
   //事件处理函数
