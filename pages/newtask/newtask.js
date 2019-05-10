e699cfcf55b1db11f720fa869cf9def67c2c66a4
dafeng <xudafeng88@qq.com>
2019-05-10 22:51:27 +0800

    (new task) 2nd commit

--- pages/newtask/newtask.js
+++ pages/newtask/newtask.js
@@ -1,56 +1,61 @@
-// pages/task/task.js
+// pages/newtask/newtask.js
 var app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
+
   data: {
-    typeName: '问卷',
-    taskList: [{
-        title: 'task1',
-        time: 'time1',
-        location: 'location1',
-        price: '1'
-      },
-      {
-        title: 'task1',
-        time: 'time1',
-        location: 'location1',
-        price: '1'
+    questionList: [{
+      type: 'choice',
+      content: 'Test',
+      options: [{
+        content: ''
       }
-    ]
+      ]
+    },
+    {
+      type: 'text',
+      content: '',
+    }
+    ], 
+    array: ['问卷', '信息收集', '招募', '物流'],
+    index: 0,
+    newTaskText: '新建任务',
+    taskname: '',
   },
-  handleReturn: function() {
-    wx.switchTab({
-      url: '/pages/index/index',
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
     })
   },
-
-  handleAddButton: function() {
-    wx.navigateTo({
-      url: '/pages/newtask/newtask',
+  
+  handleReturn: function () {
+    wx.switchTab({
+      url: '/pages/index/index',
     })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
     this.setData({
       typeName: app.globalData.currentTask
     })
@@ -59,21 +64,21 @@ Page({
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
     wx.showToast({
       title: 'loading...',
       icon: 'loading'
@@ -83,14 +88,14 @@ Page({
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
   }
 })
