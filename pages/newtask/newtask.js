4b9c436c510d98e3b4251a4c0d885032d3637753
dafeng <xudafeng88@qq.com>
2019-06-02 22:33:29 +0800

    (questionaire) fix bug

--- pages/newtask/newtask.js
+++ pages/newtask/newtask.js
@@ -7,24 +7,32 @@ Page({
    */
 
   data: {
-    questionList: [{
-      type: 'choice',
-      content: '',
-      options: [{
-        content: '',
-        index: 'A'
-      }, 
-      {
-        content: '',
-        index: 'B'
-      }
-      ]
+    task : {
+      content: {
+        questions: [{
+          type: 'choice',
+          title: '',
+          id: 1,
+          options: [{
+            content: '',
+            index: 'A'
+          },
+          {
+            content: '',
+            index: 'B'
+          }
+          ]
+        },
+        {
+          type: 'text',
+          title: '',
+          id: 2
+        }
+        ],
+      },
     },
-    {
-      type: 'text',
-      content: '',
-    }
-    ], 
+
+
 
     task_type: '问卷',
   },
@@ -43,34 +51,36 @@ Page({
 
   handleDeleteQuestion: function (e) {
     var index = e.currentTarget.dataset.index;
-    var newQuestionList = this.data.questionList;
+    var newQuestionList= this.data.task.content.questions;
     console.log(newQuestionList);
     newQuestionList.splice(index, 1);
     console.log(newQuestionList);
     this.setData({
-      questionList: newQuestionList
+      'task.content.questions': newQuestionList
     });
   },
 
   handleAddText: function (){
-    var newQuestionList = this.data.questionList;
+    var newQuestionList = this.data.task.content.questions;
     console.log(newQuestionList);
+    var new_id = newQuestionList.length == 0 ? 1 : newQuestionList[newQuestionList.length-1].id;
     newQuestionList.push({
       type: 'text',
-      content: ''
+      title: '',
+      id: new_id
     });
     console.log(newQuestionList);
     this.setData({
-      questionList: newQuestionList
+      'task.content.questions': newQuestionList
     });    
   },
 
   handleAddChoice: function () {
-    var newQuestionList = this.data.questionList;
+    var newQuestionList = this.data.task.content.questions
     console.log(newQuestionList);
     newQuestionList.push({
       type: 'choice',
-      content: '',
+      title: '',
       options: [{
         content: '',
         index: 'A'
@@ -83,14 +93,27 @@ Page({
     });
     console.log(newQuestionList);
     this.setData({
-      questionList: newQuestionList
+      'task.content.questions': newQuestionList
     });
+  },
+
+  bindQuestionTitleInput: function (e) {
+    var index = e.currentTarget.dataset.index;
+    console.log(index);
+    var newQuestionList = this.data.task.content.questions;
+    console.log(newQuestionList);
+    newQuestionList[index].title = e.detail.value;
+    console.log(newQuestionList);
+
+    this.setData({
+      'task.content.questions': newQuestionList
+    });    
   },
 
   handleAddOption: function (e) {
     var index = e.currentTarget.dataset.index;
     console.log(index);
-    var newQuestionList = this.data.questionList;
+    var newQuestionList = this.data.task.content.questions;
     console.log(newQuestionList);
     var cur_options = newQuestionList[index].options.length;
     console.log(cur_options);
@@ -101,14 +124,29 @@ Page({
     console.log(newQuestionList);
 
     this.setData({
-      questionList: newQuestionList
+      'task.content.questions': newQuestionList
+    });
+  },
+
+  bindOptionInput: function (e) {
+    var qindex = e.currentTarget.dataset.qindex;
+    var idx = e.currentTarget.dataset.idx;
+    console.log(qindex);
+    console.log(idx);
+    var newQuestionList = this.data.task.content.questions;
+    console.log(newQuestionList);
+    newQuestionList[qindex].options[idx].content = e.detail.value;
+    console.log(newQuestionList);
+
+    this.setData({
+      'task.content.questions': newQuestionList
     });
   },
 
   handleDeleteOption: function (e) {
     var index = e.currentTarget.dataset.index;
     console.log(index);
-    var newQuestionList = this.data.questionList;
+    var newQuestionList = this.data.task.content.questions;
     console.log(newQuestionList);
     var cur_options = newQuestionList[index].options.length;
     console.log(cur_options);
@@ -117,7 +155,7 @@ Page({
 
 
     this.setData({
-      questionList: newQuestionList
+      'task.content.questions': newQuestionList
     });
   },
 
